ea71282a809a7ec32ff029149de1182503acdf6a
Ryan Gorup <gorup@users.noreply.github.com>
2019-04-12 21:46:36 -0700

    Update documentation a little, needs README update, examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,17 @@
-//! The `tobytcp` library provides the `TobyMessenger` struct used for sending bi-directional messages in a `TcpStream`.
-#![feature(int_to_from_bytes)]
+//! `tobytcp` is a library used when sending messages over a buffer, typically a `TcpStream`.
+//!
+//! It uses length-prefixing to allow the receiver to differentiate different messages
 
+use std::{io, io::Write};
+
+/// Includes methods for generating the `tobytcp` prefix, or attempting to decode the length of
+/// the encoded data i.e. payload, from a buffer
 pub mod protocol;
+
+/// Writes the data, encoded as `tobytcp`, to the `Write`. Returns the total number of bytes
+/// written, equal to `data.len() + 8`
+pub fn send(data: &[u8], w: &mut dyn Write) -> Result<usize, io::Error> {
+    w.write_all(&protocol::tobytcp_prefix(data.len()))?;
+    w.write_all(data)?;
+    Ok(data.len() + 8)
+}
